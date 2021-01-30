f064d8d6fc32d6fe8cf290398ebd7bdd007c584b
kabagema <35719135+kabagema@users.noreply.github.com>
2021-01-30 22:47:11 +0200

    app set up

---
+++ my-insta/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+    
+      <div className="app__header">
+        <img
+          className="app_headerImage"
+          src="https:www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+      <h2>IG clone</h2>
+
+      {/* Header */}
+      {/* Posts */}
+    </div>
+  );
+}
+
+export default App;
