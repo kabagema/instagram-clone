5cc0cb5cf152651fa15e025b16cd42b736049f9e
kabagema <35719135+kabagema@users.noreply.github.com>
2021-01-30 23:25:06 +0200

    styling

--- my-insta/src/Post.js
+++ my-insta/src/Post.js
@@ -1,22 +1,35 @@
-import React from 'react'
+import React from "react";
 import "./Post.css";
+import { Avatar } from "@material-ui/core";
 
 function Post() {
-    return (
-        <div className="post">
-            <h3>User name</h3>
-            <img
-                className="post__image"
-                src="https://cdn-media-1.freecodecamp.org/images/1*y6C4nSvy2Woe0m7bWEn4BA.png"
-                alt=""
-            />
-            <h4>Username: caption</h4>
-            {/* header - Avatar _ name 
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt="irisofvictor"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Aka_Manto.png/146px-Aka_Manto.png"
+          // alt={username}
+        />
+        <h3>Username</h3>
+      </div>
+
+      <img
+        className="post__image"
+        src="https://cdn-media-1.freecodecamp.org/images/1*y6C4nSvy2Woe0m7bWEn4BA.png"
+        alt=""
+      />
+      <h4 className="post__text">
+        <strong>IrisofVictor:</strong>
+        First post here
+      </h4>
+      {/* header - Avatar _ name 
                 image
                 icons 
                 user name caption   */}
-        </div>
-    )
+    </div>
+  );
 }
 
-export default Post
+export default Post;
